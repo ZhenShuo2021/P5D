17f69fee94f7df9d7dae3935a1423df2f22e86db
ZhenShuo2021 <98386542+ZhenShuo2021@users.noreply.github.com>
2024-08-26 04:28:38 +0800

    Unittest: CategorizerUI

---
+++ test/test_categorizer.py
@@ -0,0 +1,89 @@
+import unittest
+from unittest.mock import Mock, patch
+
+from src.categorizer import CategorizerUI, ConfigLoader, LogManager, CategorizerFactory
+
+class LoggingTestCase(unittest.TestCase):
+    def setUp(self):
+        super().setUp()
+        self.mock_logger = Mock(spec=LogManager)
+        self.mock_logger.debug = Mock()
+        self.mock_logger.info = Mock()
+        self.mock_logger.warning = Mock()
+        self.mock_logger.error = Mock()
+        self.mock_logger.critical = Mock()
+
+
+class TestCategorizerUI(LoggingTestCase):
+    def setUp(self):
+        super().setUp()
+        self.mock_config_loader = Mock(spec=ConfigLoader)
+        self.mock_factory = Mock(spec=CategorizerFactory)
+        
+        self.test_cat1, self.test_cat2 = "category1", "category2"
+        self.mock_config_loader.get_base_paths.return_value = {"local_path": "/mock/path"}
+        self.mock_config_loader.get_combined_paths.return_value = {"path1": "/mock/path1", "path2": "/mock/path2"}
+        self.mock_config_loader.get_categories.return_value = {self.test_cat1, self.test_cat2}
+
+    @patch('pathlib.Path.exists', return_value=True)
+    def test_init_success(self, mock_exists):
+        ui = CategorizerUI(self.mock_config_loader, self.mock_logger, self.mock_factory)
+        self.assertIsInstance(ui, CategorizerUI)
+
+    @patch('pathlib.Path.exists', return_value=False)
+    def test_init_failure(self, mock_exists):
+        with self.assertRaises(FileNotFoundError):
+            CategorizerUI(self.mock_config_loader, self.mock_logger, self.mock_factory)
+
+    @patch('pathlib.Path.exists', return_value=True)
+    def test_categorize_specific(self, mock_exists):
+        """ 檢查特定分類的執行次數 """
+        # CategorizerUI 輸入 config_loader, logger, factory 參數，所以給他三個 mock
+        mock_categorizer = Mock()
+        mock_preprocess = Mock()
+        # 接下來呼叫 factory.get_categorizer ，輸出是 (preprocess, categorizer)，也新增兩個假輸出給他
+        self.mock_factory.get_categorizer.return_value = (mock_preprocess, mock_categorizer)
+
+        # 把第一個分類給 CategorizerUI.categorize 執行
+        ui = CategorizerUI(self.mock_config_loader, self.mock_logger, self.mock_factory)
+        ui.categorize(self.test_cat1)
+
+        # 確認 factory.get_categorizer 和 categorizer.categorize 都只被呼叫一次
+        self.mock_factory.get_categorizer.assert_called_once_with(self.test_cat1, ui.categories)
+        mock_categorizer.categorize.assert_called_once_with(self.test_cat1, mock_preprocess)
+
+    @patch('pathlib.Path.exists', return_value=True)
+    def test_categorize_skip(self, mock_exists):
+        """ 檢查CategorizerFactory回傳None的執行次數 """
+        self.mock_factory.get_categorizer.return_value = (False, None)
+        ui = CategorizerUI(self.mock_config_loader, self.mock_logger, self.mock_factory)
+        ui.categorize(self.test_cat1)
+
+        self.mock_factory.get_categorizer.assert_called_once_with(self.test_cat1, ui.categories)
+        self.mock_logger.debug.assert_called_once_with("Skip categorize category 'category1'.")
+
+    @patch('pathlib.Path.exists', return_value=True)
+    def test_categorize_all(self, mock_exists):
+        """ 檢查無輸入(categorize_all)的執行次數，應等於分類數量 """
+        ui = CategorizerUI(self.mock_config_loader, self.mock_logger, self.mock_factory)
+        mock_categorizer = Mock()
+        mock_preprocess = Mock()
+        self.mock_factory.get_categorizer.return_value = (mock_preprocess, mock_categorizer)
+
+        ui.categorize_all()
+
+        self.assertEqual(self.mock_factory.get_categorizer.call_count, len(self.mock_config_loader.get_categories.return_value))
+        self.assertEqual(mock_categorizer.categorize.call_count, len(self.mock_config_loader.get_categories.return_value))
+
+    @patch('pathlib.Path.exists', return_value=True)
+    def test_categorize_all_with_empty_category(self, mock_exists):
+        
+        self.mock_config_loader.get_categories.return_value = {self.test_cat1, "", self.test_cat2}
+        self.mock_factory.get_categorizer.return_value = (False, None)
+        ui = CategorizerUI(self.mock_config_loader, self.mock_logger, self.mock_factory)
+        ui.categorize_all()
+
+        self.mock_logger.critical.assert_called_once()
+
+if __name__ == '__main__':
+    unittest.main()
